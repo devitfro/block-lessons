012547ee6f2d018a3f5f810b4cf8373d41519ffd
devitfro <alinabogdanovaaaaa@gmail.com>
2026-04-07 12:47:37 +0300

    updade

--- lesson-hw9/scripts/deploy.js
+++ lesson-hw9/scripts/deploy.js
@@ -1,31 +1,31 @@
 // homework 9
-// const hre = require("hardhat");
+const hre = require("hardhat");
 
-// async function main() {
-//   const ProductManager = await hre.ethers.getContractFactory("ProductManager");
-//   const contract = await ProductManager.deploy();
+async function main() {
+  const ProductManager = await hre.ethers.getContractFactory("ProductManager");
+  const contract = await ProductManager.deploy();
 
-//   await contract.waitForDeployment();
+  await contract.waitForDeployment();
 
-//   console.log("Contract deployed to:", await contract.getAddress());
-// }
+  console.log("Contract deployed to:", await contract.getAddress());
+}
 
-// main().catch((error) => {
-//   console.error(error);
-//   process.exitCode = 1;
-// });
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
 
 // homework 10
-// const main = async () => {
-//   const Contract = await ethers.getContractFactory("PostStorage");
-//   const contract = await Contract.deploy();
+const main = async () => {
+  const Contract = await ethers.getContractFactory("PostStorage");
+  const contract = await Contract.deploy();
 
-//   await contract.waitForDeployment();
+  await contract.waitForDeployment();
 
-//   console.log("ADDRESS:", await contract.getAddress());
-// };
+  console.log("ADDRESS:", await contract.getAddress());
+};
 
-// main();
+main();
 
 // homework 11
 const main = async () => {
